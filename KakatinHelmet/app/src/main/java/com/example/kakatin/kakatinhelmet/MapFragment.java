c5b7ec48e3aaf2fabce16910c62716586f4e145d
Matti Liikala <mamilii@utu.fi>
2015-12-05 00:03:37 +0200

    Base for main activity.

---
+++ KakatinHelmet/app/src/main/java/com/example/kakatin/kakatinhelmet/MapFragment.java
@@ -0,0 +1,14 @@
+package com.example.kakatin.kakatinhelmet;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Matti on 04/12/2015.
+ */
+public class MapFragment {
+
+
+    public static Fragment newInstance() {
+        return null;
+    }
+}
