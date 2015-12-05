f032cd0f4b6591927eb09ec956dcd1a54dc60aee
Julius Rajala <juliusrajala@gmail.com>
2015-12-05 17:27:30 +0200

    Built xml stuff for app.

--- KakatinHelmet/app/src/main/java/com/example/kakatin/kakatinhelmet/MainActivity.java
+++ KakatinHelmet/app/src/main/java/com/example/kakatin/kakatinhelmet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kakatin.kakatinhelmet;
 
+import android.app.ActionBar;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -20,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FragmentManager manager = getSupportFragmentManager();
         android.support.v4.app.Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        final ActionBar actionBar = getActionBar();
+
+
+        // Specify that tabs should be displayed in the action bar.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create a tab listener that is called when the user changes tabs.
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // show the given tab
+            }
+
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // hide the given tab
+            }
+
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // probably ignore this event
+            }
+        };
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         if(fragment == null){
