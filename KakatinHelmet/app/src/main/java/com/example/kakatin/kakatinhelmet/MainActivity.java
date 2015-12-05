8a63b3e575fadadf61b0bc36c6bd69413e88f041
Julius Rajala <juliusrajala@gmail.com>
2015-12-05 18:09:34 +0200

    Statusbar color finally working, saving progress.

--- KakatinHelmet/app/src/main/java/com/example/kakatin/kakatinhelmet/MainActivity.java
+++ KakatinHelmet/app/src/main/java/com/example/kakatin/kakatinhelmet/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.kakatin.kakatinhelmet;
 
 import android.app.ActionBar;
 import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -13,35 +17,25 @@ public class MainActivity extends AppCompatActivity {
     private static final int SENSOR_FRAGMENT = 1;
     private static final int MAP_FRAGMENT = 2;
 
+    private LinearLayout statsButton;
+    private LinearLayout mapButton;
+
     private int currentFragment = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.ColorPrimaryDark));
+        }
         setContentView(R.layout.activity_main);
         FragmentManager manager = getSupportFragmentManager();
         android.support.v4.app.Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
 
-        final ActionBar actionBar = getActionBar();
-
-
-        // Specify that tabs should be displayed in the action bar.
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        // Create a tab listener that is called when the user changes tabs.
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // show the given tab
-            }
-
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // hide the given tab
-            }
-
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // probably ignore this event
-            }
-        };
+        statsButton = (LinearLayout)
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         if(fragment == null){
