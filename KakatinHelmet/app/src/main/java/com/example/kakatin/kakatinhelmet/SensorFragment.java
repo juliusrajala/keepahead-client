a25504a3fcd62bb7b6957dff8fb09dddef2954d0
Julius Rajala <juliusrajala@gmail.com>
2015-12-05 16:32:54 +0200

    Changes to SensorFragment.

--- KakatinHelmet/app/src/main/java/com/example/kakatin/kakatinhelmet/SensorFragment.java
+++ KakatinHelmet/app/src/main/java/com/example/kakatin/kakatinhelmet/SensorFragment.java
@@ -7,27 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SensorFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SensorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SensorFragment extends Fragment {
     private static final String TAG = SensorFragment.class.getSimpleName();
 
+    private TextView speedStat;
+    private TextView tempStat;
+    private TextView localeStat;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment SensorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    private int mTemp;
+    private int mSpeed;
+    private String mLocale;
+
     public static SensorFragment newInstance() {
         SensorFragment fragment = new SensorFragment();
         Bundle args = new Bundle();
@@ -44,7 +37,12 @@ public class SensorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sensor, container, false);
+        View v = inflater.inflate(R.layout.fragment_sensor, container, false);
+        speedStat = (TextView)v.findViewById(R.id.speed_stat);
+        tempStat = (TextView)v.findViewById(R.id.temp_stat);
+        localeStat = (TextView)v.findViewById(R.id.locale_stat);
+
+        return v;
     }
 
     @Override
